820f9f2c3ee6c164457ac7c0d5821436886670af
AlejandroTC <aletamayo-02@hotmail.com>
2023-04-25 09:09:36 -0600

    cambiarelblob

--- Proyecto/php/misRecetas.php
+++ Proyecto/php/misRecetas.php
@@ -6,31 +6,21 @@ $dbname = "recetasDB";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Establecer el modo de error PDO a excepción
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Obtener la sentencia SQL enviada por el cliente
+	// Establecer el modo de error PDO a excepción
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    header("Content-Type: application/json");
     $sql = $_POST['sql'];
-
-    // Ejecutar la sentencia SQL en la base de datos
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-
-    // Obtener los datos del resultado y convertirlos a un array asociativo
-    $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    // Recorrer los resultados y obtener la imagen en formato base64
-    foreach ($resultados as &$resultado) {
-        $imagen = $resultado['imagen'];
-        $resultado['imagen'] = base64_encode($imagen);
+    $resultado = $conn->query($sql);
+    if ($resultado && mysqli_num_rows($resultado) > 0) {
+        $fila = mysqli_fetch_assoc($resultado);
+        $imagen = $fila["imagen"];
+        $tipo = $imagen->type;
+        $datos = base64_encode(file_get_contents($imagen->tmp_name));
+        $url = "data:$tipo;base64,$datos";
+        echo json_encode(array("url" => $url)); 
+    } else {
+        echo json_encode(array("error" => "No se encontró ninguna imagen"));
     }
-    
-    // Enviar respuesta al cliente en formato JSON
-    header('Content-Type: application/json');
-    echo json_encode($resultados);
-
-    // Cerrar conexión a la base de datos
-    $conn = null;
 } catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
